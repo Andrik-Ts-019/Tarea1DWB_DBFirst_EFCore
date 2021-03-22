8f6047cc295be5c36c9f68fc47c80eef1e9aaf32
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2021-03-21 20:03:35 -0600

    Segunda refactorizacion

--- Tarea1DWB_DBFirst_EFCore/Program.cs
+++ Tarea1DWB_DBFirst_EFCore/Program.cs
@@ -6,23 +6,18 @@ namespace Tarea1DWB_DBFirst_EFCore
 {
     class Program
     {
-        //Objeto por el cual accedemos a las entidades de la BD
         public static NorthwindContext dbContext = new NorthwindContext();
 
-        //Mostramos todos los productos
-        public static void selectProducts()
+        public static void SelectProducts()
         {
-            //Variable que recibe a la entidad Product pero no materializada. Select * From Products:
             var productsQuery = GetAllProducts();
 
-            //Materialización de productQuery
-            var outPut = productsQuery.ToList();
+            //var outPut = productsQuery.ToList();
         }
 
         //Mostramos los datos de un producto en especial
-        public static void selectProducts(string productName)
+        public static void SelectProduct(string productName)
         {
-            //Variable que recibe a la entidad Products pero no materializada.
             var productQuery = GetAllProducts().Where(w => w.ProductName == productName).Select(p => new
             {
                 ID = p.ProductId,
@@ -33,81 +28,117 @@ namespace Tarea1DWB_DBFirst_EFCore
                 Continuidad = p.Discontinued
             });
 
-            //Materialización de productQuery
-            var outPut = productQuery.ToList();
+            //var outPut = productQuery.ToList();
         }
 
-        //Mostramos a todos los empleados
-        public static void selectEmployees()
+        public static void UpdateProductName(int productId, string productName)
         {
-            //Variable que recibe a la entidad Employees pero no materializada. Select * From Employees:
-            var employeesQuery = GetAllEmployees();
-
-            //Materialización de employeesQuery
-            var outPut = employeesQuery.ToList();
-        }
-
-        //Mostramos los datos de un empleado en especial
-        public static void selectEmployees(string employeeFirstName, string employeeLastName)
-        {
-            //Variable que recibe a la entidad Employees pero no materializada.
-            var employeesQuery = GetAllEmployees().
-                Where(w => (w.FirstName == employeeFirstName) && (w.LastName == employeeLastName)).Select(e => new
-            {
-                ID = e.EmployeeId,
-                Nombre = e.FirstName,
-                Apellido = e.LastName,
-                Puesto = e.Title,
-                Dirección = e.Address,
-                Telefono = e.HomePhone
-            });
-
-            //Materialización de employeesQuery
-            var outPut = employeesQuery.ToList();
-        }
-
-        //Actualización del nombre o apellido de un empleado
-        public static void updateEmployeeName(int employeeID, string employeeName, int isFirstName = 1)
-        {
-            //Variable que guarda el dato a acualizar.
-            var currentEmployee = GetAllEmployees().Where(e => e.EmployeeId == employeeID).FirstOrDefault();
-
-            if (currentEmployee == null)
-                throw new Exception("\nID de empleado no escontrado");
-            
-            if (isFirstName == 1)
-                //Actualizamos FistName en currentEmployee
-                currentEmployee.FirstName = employeeName;
-            else
-                //Actualizamos Lastame en currentEmployee
-                currentEmployee.LastName = employeeName;
-
-            //Guardamos cambios pues currentEmployee está enlazado a dbContext
-            dbContext.SaveChanges();
-        }
-
-        //Actualización del nombre de un producto
-        public static void updateProductName(int productId, string productName)
-        {
-            Products currentProduct = getProductByID(productId);
+            Products currentProduct = GetProductByID(productId);
 
             if (currentProduct == null)
                 throw new Exception("ID de producto no escontrado");
 
-            //Actualizamos ProductName en currentProduct
             currentProduct.ProductName = productName;
 
-            //Guardamos cambios pues currentProduct está enlazado a dbContext
             dbContext.SaveChanges();
         }
 
-        //Nuevo registro en Products
-        public static void addProduct(string productName, int? supplierId, int? categoryId, decimal? unitPrice, short? unitsInStock, short? unitsOnOrder, short? reorderLevel, bool discontinued = false, string quantityPerUnit = "")
+        public static void AddProduct(string productName, int? supplierId, int? categoryId, decimal? unitPrice, short? unitsInStock, short? unitsOnOrder, short? reorderLevel, bool discontinued = false, string quantityPerUnit = "")
         {
-            //Variable que guarda el nuevo producto.
+            AddNewProduct(productName, supplierId, categoryId, unitPrice, unitsInStock, unitsOnOrder, reorderLevel, discontinued, quantityPerUnit);
+        }
+
+        public static void DeleteProduct(int productId)
+        {
+            var dProduct = GetProductByID(productId);
+
+            dbContext.Products.Remove(dProduct);
+            dbContext.SaveChanges();
+        }
+
+        //Devuelve el producto, cliente y empleado por OrderID
+        public static void OrderProducts(int orderId)
+        {
+            var ordersQuery = GetOrderById(orderId).Select(o => new
+                {
+                    Cliente = o.Customer.CompanyName,
+                    Empleado = o.Employee.FirstName,
+                    Productos = o.OrderDetails.Select(ordDet => ordDet.Product.ProductName)
+                });
+
+            var outPut = ordersQuery.ToList();
+        }
+
+        public static void SelectEmployees()
+        {
+            var employeesQuery = GetAllEmployees();
+
+            var outPut = employeesQuery.ToList();
+        }
+
+        //Mostramos los datos de un empleado en especial
+        public static void SelectEmployee(string employeeFirstName, string employeeLastName)
+        {
+            var employeeQuery = GetAllEmployees().
+                Where(w => (w.FirstName == employeeFirstName) && (w.LastName == employeeLastName)).Select(e => new
+                {
+                    ID = e.EmployeeId,
+                    Nombre = e.FirstName,
+                    Apellido = e.LastName,
+                    Puesto = e.Title,
+                    Dirección = e.Address,
+                    Telefono = e.HomePhone
+                });
+
+            var outPut = employeeQuery.ToList();
+        }
+
+        public static void UpdateEmployeeName(int employeeID, string employeeName, int isFirstName = 1)
+        {
+            var currentEmployee = GetAllEmployees().Where(e => e.EmployeeId == employeeID).FirstOrDefault();
+
+            if (currentEmployee == null)
+                throw new Exception("\nID de empleado no escontrado");
+
+            if (isFirstName == 1)
+                currentEmployee.FirstName = employeeName;
+            else
+                currentEmployee.LastName = employeeName;
+
+            dbContext.SaveChanges();
+        }
+
+        //Métodos privados
+        #region privateMethods
+        private static IQueryable<Products> GetAllProducts()
+        {
+            return dbContext.Products.Select(p => p);
+        }
+
+        private static IQueryable<Employees> GetAllEmployees()
+        {
+            return dbContext.Employees.Select(e => e);
+        }
+
+        private static IQueryable<Orders> GetAllOrders()
+        {
+            return dbContext.Orders;
+        }
+
+        private static Products GetProductByID(int productId)
+        {
+            return GetAllProducts().Where(prod => prod.ProductId == productId).FirstOrDefault();
+        }
+
+        private static IQueryable<Orders> GetOrderById(int orderId)
+        {
+            return GetAllOrders().Where(w => w.OrderId == orderId);
+        }
+
+        private static void AddNewProduct(string productName, int? supplierId, int? categoryId, decimal? unitPrice, short? unitsInStock, short? unitsOnOrder, short? reorderLevel, bool discontinued, string quantityPerUnit)
+        {
             var newProduct = new Products();
 
-            //Asignamos datos
             newProduct.ProductName = productName;
             newProduct.SupplierId = supplierId;
             newProduct.CategoryId = categoryId;
@@ -121,59 +152,11 @@ namespace Tarea1DWB_DBFirst_EFCore
             dbContext.Products.Add(newProduct);
             dbContext.SaveChanges();
         }
-
-        //Nuevo registro en Products
-        public static void deleteProduct(int productId)
-        {
-            //Variable que guarda el producto a borrar.
-            var dProduct = getProductByID(productId);
-
-            //Borramos registro
-            dbContext.Products.Remove(dProduct);
-            //Guardamos los cambios
-            dbContext.SaveChanges();
-        }
-
-        //Devuelve el producto, cliente y empleado por OrderID
-        public static void orderProducts(int orderId)
-        {
-            //Obtenermos el pedido
-            var ordersQuery = GetAllOrders().Where(w => w.OrderId == orderId)
-                .Select(ord => new
-                {
-                    Cliente = ord.Customer.CompanyName,
-                    Empleado = ord.Employee.FirstName,
-                    Productos = ord.OrderDetails.Select(ordDet => ordDet.Product.ProductName)
-                });
-        }
-
-        //Métodos privados
-        #region HelperMethods
-        private static IQueryable<Products> GetAllProducts()
-        {
-            return dbContext.Products.Select(p => p);
-        }
-
-        private static IQueryable<Employees> GetAllEmployees()
-        {
-            return dbContext.Employees.Select(e => e);
-        }
-
-        private static IQueryable<Orders> GetAllOrders()
-        {
-            return dbContext.Orders.Select(o => o);
-        }
-
-        private static Products getProductByID(int productId)
-        {
-            return dbContext.Products.Where(prod => prod.ProductId == productId).FirstOrDefault();
-        }
         #endregion
 
         static void Main(string[] args)
         {
             //addProduct(productName: "Agua loca", unitsInStock: 6, unitsOnOrder: 0, unitPrice: 30, quantityPerUnit: "20 boxes", supplierId: null, categoryId: null, reorderLevel: null);
-            //joinProductClientEmployee(10248);
             Console.WriteLine("Hello World!");
         }
     }
