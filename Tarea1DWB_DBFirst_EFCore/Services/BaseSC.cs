b55013f8892d32a11d4fda4d2c98d4f84cf12047
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2021-03-21 22:17:21 -0600

    Clase padre

---
+++ Tarea1DWB_DBFirst_EFCore/Services/BaseSC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tarea1DWB_DBFirst_EFCore.DataAccess;
+
+namespace Tarea1DWB_DBFirst_EFCore.Services
+{
+    public class BaseSC
+    {
+        public NorthwindContext dbContext = new NorthwindContext();
+    }
+}
