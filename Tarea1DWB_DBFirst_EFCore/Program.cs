5acafa4c8160020cd505e1985bae47d48a6865f9
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2021-03-21 19:14:56 -0600

    Primera refactorizacion

--- Tarea1DWB_DBFirst_EFCore/Program.cs
+++ Tarea1DWB_DBFirst_EFCore/Program.cs
@@ -6,30 +6,24 @@ namespace Tarea1DWB_DBFirst_EFCore
 {
     class Program
     {
+        //Objeto por el cual accedemos a las entidades de la BD
+        public static NorthwindContext dbContext = new NorthwindContext();
+
         //Mostramos todos los productos
         public static void selectProducts()
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-            
             //Variable que recibe a la entidad Product pero no materializada. Select * From Products:
-            var productsQuery = northwindContext.Products.Select(element => element);
+            var productsQuery = GetAllProducts();
 
             //Materialización de productQuery
             var outPut = productsQuery.ToList();
-
-            //Imprimir el nombre de los productos en la tabla Products
-            outPut.ForEach(i => Console.WriteLine("Producto: " + i.ProductName));
         }
 
         //Mostramos los datos de un producto en especial
         public static void selectProducts(string productName)
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-
             //Variable que recibe a la entidad Products pero no materializada.
-            var productQuery = northwindContext.Products.Where(w => w.ProductName == productName).Select(p => new
+            var productQuery = GetAllProducts().Where(w => w.ProductName == productName).Select(p => new
             {
                 ID = p.ProductId,
                 Producto = p.ProductName,
@@ -41,35 +35,23 @@ namespace Tarea1DWB_DBFirst_EFCore
 
             //Materialización de productQuery
             var outPut = productQuery.ToList();
-
-            //Imprimir el nombre de los productos en la tabla Products
-            outPut.ForEach(i => Console.WriteLine("Producto: " + i.Producto));
         }
 
         //Mostramos a todos los empleados
         public static void selectEmployees()
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-
             //Variable que recibe a la entidad Employees pero no materializada. Select * From Employees:
-            var employeesQuery = northwindContext.Employees.Select(element => element);
+            var employeesQuery = GetAllEmployees();
 
             //Materialización de employeesQuery
             var outPut = employeesQuery.ToList();
-
-            //Imprimir el id y nombre completo de los empleados registrados en la tabla Employees
-            outPut.ForEach(i => Console.WriteLine("ID: " + i.EmployeeId + " " + i.FirstName + " " + i.LastName));
         }
 
         //Mostramos los datos de un empleado en especial
         public static void selectEmployees(string employeeFirstName, string employeeLastName)
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-
             //Variable que recibe a la entidad Employees pero no materializada.
-            var employeesQuery = northwindContext.Employees.
+            var employeesQuery = GetAllEmployees().
                 Where(w => (w.FirstName == employeeFirstName) && (w.LastName == employeeLastName)).Select(e => new
             {
                 ID = e.EmployeeId,
@@ -82,39 +64,32 @@ namespace Tarea1DWB_DBFirst_EFCore
 
             //Materialización de employeesQuery
             var outPut = employeesQuery.ToList();
-
-            //Imprimir el id y nombre completo de los empleados registrados en la tabla Employees
-            outPut.ForEach(i => Console.WriteLine("ID: " + i.ID + " " + i.Nombre + " " + i.Puesto + " " + i.Dirección));
         }
 
-        //Actualización del nombre oapellido de un empleado
-        public static void updateEmployeeName(int employeeID, string employeeFLName, int isFirstName = 1)
+        //Actualización del nombre o apellido de un empleado
+        public static void updateEmployeeName(int employeeID, string employeeName, int isFirstName = 1)
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
             //Variable que guarda el dato a acualizar.
-            var currentEmployee = northwindContext.Employees.Where(emp => emp.EmployeeId == employeeID).FirstOrDefault();
+            var currentEmployee = GetAllEmployees().Where(e => e.EmployeeId == employeeID).FirstOrDefault();
 
             if (currentEmployee == null)
                 throw new Exception("\nID de empleado no escontrado");
             
             if (isFirstName == 1)
                 //Actualizamos FistName en currentEmployee
-                currentEmployee.FirstName = employeeFLName;
+                currentEmployee.FirstName = employeeName;
             else
                 //Actualizamos Lastame en currentEmployee
-                currentEmployee.LastName = employeeFLName;
+                currentEmployee.LastName = employeeName;
 
-            //Guardamos cambios pues currentEmployee está enlazado a northwindContext
-            northwindContext.SaveChanges();
+            //Guardamos cambios pues currentEmployee está enlazado a dbContext
+            dbContext.SaveChanges();
         }
 
         //Actualización del nombre de un producto
         public static void updateProductName(int productId, string productName)
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-            Products currentProduct = getProductByID(productId, northwindContext);
+            Products currentProduct = getProductByID(productId);
 
             if (currentProduct == null)
                 throw new Exception("ID de producto no escontrado");
@@ -122,21 +97,13 @@ namespace Tarea1DWB_DBFirst_EFCore
             //Actualizamos ProductName en currentProduct
             currentProduct.ProductName = productName;
 
-            //Guardamos cambios pues currentProduct está enlazado a northwindContext
-            northwindContext.SaveChanges();
-        }
-
-        private static Products getProductByID(int productId, NorthwindContext northwindContext)
-        {
-            return northwindContext.Products.Where(prod => prod.ProductId == productId).FirstOrDefault();
+            //Guardamos cambios pues currentProduct está enlazado a dbContext
+            dbContext.SaveChanges();
         }
 
         //Nuevo registro en Products
         public static void addProduct(string productName, int? supplierId, int? categoryId, decimal? unitPrice, short? unitsInStock, short? unitsOnOrder, short? reorderLevel, bool discontinued = false, string quantityPerUnit = "")
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-            
             //Variable que guarda el nuevo producto.
             var newProduct = new Products();
 
@@ -151,51 +118,62 @@ namespace Tarea1DWB_DBFirst_EFCore
             newProduct.ReorderLevel = reorderLevel;
             newProduct.Discontinued = discontinued;
 
-            northwindContext.Products.Add(newProduct);
-            northwindContext.SaveChanges();
+            dbContext.Products.Add(newProduct);
+            dbContext.SaveChanges();
         }
 
         //Nuevo registro en Products
         public static void deleteProduct(int productId)
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-
             //Variable que guarda el producto a borrar.
-            var dProduct = getProductByID(productId,northwindContext);
+            var dProduct = getProductByID(productId);
 
             //Borramos registro
-            northwindContext.Products.Remove(dProduct);
-
-            northwindContext.SaveChanges();
+            dbContext.Products.Remove(dProduct);
+            //Guardamos los cambios
+            dbContext.SaveChanges();
         }
 
         //Devuelve el producto, cliente y empleado por OrderID
-        public static void joinProductClientEmployee(int orderId)
+        public static void orderProducts(int orderId)
         {
-            //Objeto por el cual accedemos a las entidades de la BD
-            NorthwindContext northwindContext = new NorthwindContext();
-
             //Obtenermos el pedido
-            var ordersQuery = northwindContext.Orders.Where(w => w.OrderId == orderId)
+            var ordersQuery = GetAllOrders().Where(w => w.OrderId == orderId)
                 .Select(ord => new
                 {
                     Cliente = ord.Customer.CompanyName,
                     Empleado = ord.Employee.FirstName,
                     Productos = ord.OrderDetails.Select(ordDet => ordDet.Product.ProductName)
                 });
-
-            //Materialización de productQuery
-            var output = ordersQuery.ToList();
-
-            //Imprimir el nombre de los productos en la tabla Products
-            //outPut.ForEach(i => Console.WriteLine("Producto: " + i.OrderId));
         }
+
+        //Métodos privados
+        #region HelperMethods
+        private static IQueryable<Products> GetAllProducts()
+        {
+            return dbContext.Products.Select(p => p);
+        }
+
+        private static IQueryable<Employees> GetAllEmployees()
+        {
+            return dbContext.Employees.Select(e => e);
+        }
+
+        private static IQueryable<Orders> GetAllOrders()
+        {
+            return dbContext.Orders.Select(o => o);
+        }
+
+        private static Products getProductByID(int productId)
+        {
+            return dbContext.Products.Where(prod => prod.ProductId == productId).FirstOrDefault();
+        }
+        #endregion
 
         static void Main(string[] args)
         {
             //addProduct(productName: "Agua loca", unitsInStock: 6, unitsOnOrder: 0, unitPrice: 30, quantityPerUnit: "20 boxes", supplierId: null, categoryId: null, reorderLevel: null);
-            joinProductClientEmployee(10248);
+            //joinProductClientEmployee(10248);
             Console.WriteLine("Hello World!");
         }
     }
