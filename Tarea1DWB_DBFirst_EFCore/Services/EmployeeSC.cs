068d915b8cda15a51e2d9295cd9858ac873e4847
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2021-03-21 21:57:07 -0600

    Servicio Employee

--- Tarea1DWB_DBFirst_EFCore/Services/EmployeeSC.cs
+++ Tarea1DWB_DBFirst_EFCore/Services/EmployeeSC.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Tarea1DWB_DBFirst_EFCore.DataAccess;
 
 namespace Tarea1DWB_DBFirst_EFCore.Services
 {
-    class EmployeeSC
+    public class EmployeeSC
     {
+        public NorthwindContext dbContext = new NorthwindContext();
+
+        #region Methods
+        public void UpdateEmployeeName(int employeeID, string employeeName, int isFirstName = 1)
+        {
+            var currentEmployee = GetAllEmployees().Where(e => e.EmployeeId == employeeID).FirstOrDefault();
+
+            if (currentEmployee == null)
+                throw new Exception("\nID de empleado no escontrado");
+
+            if (isFirstName == 1)
+                currentEmployee.FirstName = employeeName;
+            else
+                currentEmployee.LastName = employeeName;
+
+            dbContext.SaveChanges();
+        }
+        #endregion
+
+        #region HelperMethods
+        public IQueryable<Employees> GetAllEmployees()
+        {
+            return dbContext.Employees.Select(e => e);
+        }
+        #endregion
     }
 }
