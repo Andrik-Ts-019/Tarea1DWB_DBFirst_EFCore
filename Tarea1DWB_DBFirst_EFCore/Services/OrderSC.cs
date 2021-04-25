419b7fdcd31493ee85b12b5c2186da3a003e037a
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2021-04-25 17:37:43 -0500

    Nuevo metodo

--- Tarea1DWB_DBFirst_EFCore/Services/OrderSC.cs
+++ Tarea1DWB_DBFirst_EFCore/Services/OrderSC.cs
@@ -8,15 +8,31 @@ namespace Tarea1DWB_DBFirst_EFCore.Services
 {
     public class OrderSC : BaseSC
     {
+        #region
+        public void UpdateShipAdressOrder(int orderId, string shipAdress)
+        {
+            var currentOrder = GetOrderById(orderId);
+
+            if (currentOrder == null)
+                throw new Exception("\nID de Orden no escontrado");
+
+            currentOrder.ShipAddress = shipAdress;
+
+            dbContext.SaveChanges();
+        }
+        #endregion
+
         #region HelperMethods
+        //GET
         public IQueryable<Orders> GetAllOrders()
         {
             return dbContext.Orders;
         }
 
-        public IQueryable<Orders> GetOrderById(int orderId)
+        //GET
+        public Orders GetOrderById(int orderId)
         {
-            return GetAllOrders().Where(w => w.OrderId == orderId);
+            return GetAllOrders().Where(w => w.OrderId == orderId).FirstOrDefault(); ;
         }
         #endregion
     }
