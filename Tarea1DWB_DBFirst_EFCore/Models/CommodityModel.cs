ddb76572e8113b3497e9a72994db3076b76e2187
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2021-04-25 23:58:26 -0500

    Modelos

---
+++ Tarea1DWB_DBFirst_EFCore/Models/CommodityModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBFirst.Models
+{
+    public class CommodityModel
+    {
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public decimal? Price { get; set; }
+        public short Stock {get; set;}
+    }
+}
---
+++ Tarea1DWB_DBFirst_EFCore/Models/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBFirst.Models
+{
+    public class OrderModel
+    {
+        public string ClientID { get; set; }
+        public int? ClerkID { get; set; }
+        public DateTime? CashOrderDate { get; set; }
+        public DateTime? RequestedDate { get; set; }
+        public DateTime? SentDate { get; set; }
+        public int? ShipperID { get; set; }
+        public decimal? Cargo { get; set; }
+        public string TransportName { get; set; }
+        public string TransportAddress { get; set; }
+        public string TransportCity { get; set; }
+    }
+}
